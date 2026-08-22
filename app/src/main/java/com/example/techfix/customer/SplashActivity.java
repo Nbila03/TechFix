0ae7b642689f886a168382e11310dd71e8903e64
Sewmini Nawodya <snawodya196@gmail.com>
2026-08-23 01:51:14 +0530

    Implement splash activity

---
+++ app/src/main/java/com/example/techfix/customer/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.techfix.customer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.techfix.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+
+            Intent intent = new Intent(
+                    SplashActivity.this,
+                    LoginActivity.class
+            );
+
+            startActivity(intent);
+            finish();
+
+        }, 1500);
+    }
+}
