89b0a874f4f6801650bbbadca022c9004f944abd
Nabila <17nabilafahim@gmail.com>
2026-08-23 15:01:55 +0530

    implement technicians retr from firrestore

--- app/src/main/java/com/example/techfix/firebase/TechnicianRepository.java
+++ app/src/main/java/com/example/techfix/firebase/TechnicianRepository.java
@@ -1,10 +1,38 @@
 package com.example.techfix.firebase;
 
+import com.example.techfix.model.Technician;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TechnicianRepository {
+
     private final FirebaseFirestore db;
+
     public TechnicianRepository() {
         db = FirebaseFirestore.getInstance();
+    }
+
+    public interface OnTechniciansLoaded {
+        void onLoaded(List<Technician> technicians);
+    }
+
+    public interface OnErrorCallback {
+        void onError(Exception e);
+    }
+
+    public void getAllTechnicians(OnTechniciansLoaded onLoaded, OnErrorCallback onError) {
+        db.collection("technicians")
+                .get()
+                .addOnSuccessListener(result -> {
+                    List<Technician> technicians = new ArrayList<>();
+                    for (var document : result) {
+                        Technician t = document.toObject(Technician.class);
+                        technicians.add(t);
+                    }
+                    onLoaded.onLoaded(technicians);
+                })
+                .addOnFailureListener(onError::onError);
     }
 }
