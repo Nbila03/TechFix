7a280b190ccf8bf392b4df850a1bc60d82a8d7d0
Shaima <shaimafathima583@gmail.com>
2026-08-23 08:28:59 +0530

    Implemented SparePartRepository that defines CRUD

---
+++ app/src/main/java/com/example/techfix/firebase/SparePartRepository.java
@@ -0,0 +1,91 @@
+
+package com.example.techfix.firebase;
+
+import com.example.techfix.model.SparePart;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SparePartRepository {
+
+    // Firebase database
+    private FirebaseFirestore db;
+
+    public SparePartRepository() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    // ADD SPARE PART
+
+    public void addSparePart(SparePart part) {
+
+        String id = String.valueOf(part.getPartId());
+
+        db.collection("spareParts")
+                .document(id)
+                .set(part);
+    }
+
+    // GET ALL SPARE PARTS
+
+    public void getSpareParts(
+            OnSparePartsLoaded listener) {
+
+        db.collection("spareParts")
+                .get()
+                .addOnSuccessListener(result -> {
+
+                    List<SparePart> parts =
+                            new ArrayList<>();
+
+                    for (var document : result) {
+
+                        SparePart part =
+                                document.toObject(
+                                        SparePart.class
+                                );
+
+                        parts.add(part);
+                    }
+
+                    listener.onLoaded(parts);
+                });
+    }
+
+    // UPDATE STOCK
+
+    public void updateStock(
+            int partId,
+            int newQuantity) {
+
+        db.collection("spareParts")
+                .document(String.valueOf(partId))
+                .update(
+                        "quantity",
+                        newQuantity
+                );
+    }
+
+
+// DELETE SPARE PART
+
+    public void deleteSparePart(int partId) {
+
+        db.collection("spareParts")
+                .document(String.valueOf(partId))
+                .delete();
+    }
+
+
+
+    // LISTENER
+
+    public interface OnSparePartsLoaded {
+
+        void onLoaded(
+                List<SparePart> parts
+        );
+    }
+}
+
