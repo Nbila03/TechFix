e9ba810d7fb4c20e15f91d8abb926056f75cf262
Shaima <shaimafathima583@gmail.com>
2026-08-24 04:39:18 +0530

    Updated main

--- app/src/main/java/com/example/techfix/MainActivity.java
+++ app/src/main/java/com/example/techfix/MainActivity.java
@@ -1,26 +1,13 @@
 package com.example.techfix;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.techfix.management.AdminDashboardActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent =
-                new Intent(
-                        MainActivity.this,
-                        AdminDashboardActivity.class
-                );
-
-        startActivity(intent);
-
-        finish();
     }
 }
